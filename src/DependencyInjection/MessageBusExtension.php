d947e49e22c04849f517aa17f03c804bae9bfdd1
Kamil Pietrzkiewicz <sargitcom@gmail.com>
2025-12-21 16:12:44 +0100

    [WIP] fix namespace

--- src/DependencyInjection/MessageBusExtension.php
+++ src/DependencyInjection/MessageBusExtension.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace SerpSamuraiIo\MessageBus;
+namespace SerpSamuraiIo\MessageBus\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class MessageBusExtension  extends Extension
+class MessageBusExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
