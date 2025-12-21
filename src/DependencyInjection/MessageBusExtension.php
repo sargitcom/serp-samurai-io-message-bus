2a165d09953dbe3ac068763baf7cd10963044b16
Kamil Pietrzkiewicz <sargitcom@gmail.com>
2025-12-21 16:20:19 +0100

    [WIP] add loading of the services

--- src/DependencyInjection/MessageBusExtension.php
+++ src/DependencyInjection/MessageBusExtension.php
@@ -3,12 +3,15 @@
 namespace SerpSamuraiIo\MessageBus\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
 
 class MessageBusExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        var_dump('We\'re alive!');die;
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
     }
 }
