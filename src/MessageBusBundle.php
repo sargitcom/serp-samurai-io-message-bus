b7e0ab9d3188fe01c8c1a2c4a95333c534012d9d
Kamil Pietrzkiewicz <sargitcom@gmail.com>
2026-02-15 18:29:26 +0100

    feat: update bundle to symfony 8

--- src/MessageBusBundle.php
+++ src/MessageBusBundle.php
@@ -2,9 +2,21 @@
 
 namespace SerpSamuraiIo\MessageBus;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Exception;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class MessageBusBundle  extends Bundle
+class MessageBusBundle  extends AbstractBundle
 {
-
+    /**
+     * @throws Exception
+     */
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $loader = new YamlFileLoader($builder, new FileLocator(__DIR__.'/Resources/config'));
+        $loader->load('services.yaml');
+    }
 }
